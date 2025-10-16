01164cf9648dc3106a202f74a08c02c1a4956678
taingchhay <sophaltangchhay@gmail.com>
2025-10-16 14:26:09 +0700

    modified EX1

--- EX1/EX-1.dart
+++ EX1/EX-1.dart
@@ -1,13 +1,18 @@
 enum Skill { FLUTTER, DART, OTHER }
 
+//class employee
 class Employee {
-  final String _name;
+
+  //skills: A list of skills 
+  final String _name; // cannot change next time
   final double _baseSalary;
   final Skill _skills;
   final Address _address;
+
+  //add year of experience
   final int _yearsOfExperience;
 
-  // name constructor
+  // main constructor
   Employee({
     required String name,
     required double baseSalary,
@@ -21,7 +26,7 @@ class Employee {
        _yearsOfExperience = yearsOfExperience;
 
   // Named constructor for mobile developer
-  Employee.mobileDeveloper( Address address, int yearsOfExperience)
+  Employee.mobileDeveloper(Address address, int yearsOfExperience)
     : this(
         name: 'mengheng',
         baseSalary: 40000,
@@ -37,34 +42,32 @@ class Employee {
   Address get address => _address;
   int get yearsOfExperience => _yearsOfExperience;
 
+  //compute
+  double computeSalary() {
+    double totalSalary = _baseSalary;
+    if (_skills == Skill.FLUTTER) {
+      totalSalary = totalSalary + 5000 + (yearsOfExperience * 2000);
+    } else if (_skills == Skill.DART) {
+      totalSalary = totalSalary + 3000 + (yearsOfExperience * 2000);
+    } else {
+      totalSalary = totalSalary + 1000 + (yearsOfExperience * 2000);
+    }
+    //print('total salary is ${ _baseSalary + }');
+    return totalSalary;
+  }
+  //print detail
+
   void printDetails() {
     print(
       'Employee: $_name\n'
-      'Base Salary: \$$_baseSalary\n'
+      'Total Salary: \$${computeSalary()}\n'
       'Address: ${_address.street} ${_address.city} ${_address.zipCode}\n'
       'Experience: $_yearsOfExperience years\n'
       'Skill: $_skills',
     );
   }
-
-  void computeSalary() {
-    if (_skills == Skill.FLUTTER) {
-      print(
-        ' total salary is ${_baseSalary + 5000 + (yearsOfExperience * 2000)}',
-      );
-    } else if (_skills == Skill.DART) {
-      print(
-        ' total salary is ${_baseSalary + 3000 + (yearsOfExperience * 2000)}',
-      );
-    } else {
-      print(
-        ' total salary is ${_baseSalary + 1000 + (yearsOfExperience * 2000)}',
-      );
-    }
-    //print('total salary is ${ _baseSalary + }');
-  }
 }
-
+//address: An Address class that contains street, city, and zipCode attributes
 class Address {
   final String _street;
   final String _city;
@@ -80,24 +83,20 @@ class Address {
 
 void main() {
   // Create employee using main constructor
-  final emp2 = Employee(
-    name: 'sokea',
-    baseSalary: 40000.00,
-    skills: Skill.OTHER,
-    address: Address('Street 6A', 'Phnom Penh', 'A11'),
-    yearsOfExperience: 10,
-  );
+  // final emp2 = Employee(
+  //   name: 'sokea',
+  //   baseSalary: 40000.00,
+  //   skills: Skill.FLUTTER,
+  //   address: Address('Street 6A', 'Phnom Penh', 'A11'),
+  //   yearsOfExperience: 10,
+  // );
 
   final emp1 = Employee.mobileDeveloper(
-    
-    Address('123', 'pp', 'abc'),
-    20,
-    
-  );
+    Address('123', 'pp', 'abc'), 
+    1);
   //emp1._skills;
-  
+  emp1.printDetails();
 
-  print(emp2._baseSalary);
-  //emp1.computeSalary();
-  emp2.printDetails();
+  // print(emp2._baseSalary);
+  //emp2.printDetails();
 }
